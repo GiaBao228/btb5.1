bbee199f51679cbddb40d7e77a73f6aefa5e9e4b
Bao Gia <nguyenducgiabao18@outlook.com>
2024-12-09 23:41:24 +0700

    Add project files.

---
+++ btb5.1/Models/ProductImage.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace btb5._1.Models
+{
+    public class ProductImage
+    {
+        public int Id { get; set; }
+        public string Url { get; set; }
+        public int ProductId { get; set; }
+        public Product? Product { get; set; }
+    }
+}
---
+++ btb5.1/Repositories/ICategoryRepository.cs
@@ -0,0 +1,14 @@
+﻿using btb5._1.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace btb5._1.Repositories
+{
+    public interface ICategoryRepository
+    {
+        Task<IEnumerable<Category>> GetAllAsync();
+        Task<Category> GetByIdAsync(int id);
+        Task AddAsync(Category category);
+        Task UpdateAsync(Category category);
+        Task DeleteAsync(int id);
+    }
+}
